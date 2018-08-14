c80ad8a337f6f8bbb71e8171c01b6e9d8fa7e823
Joost <joost.w.m@live.nl>
2018-08-14 21:41:15 +0200

    created the bare structure for the function that should reproduce the 6 regression tables in the MRW 1992 paper

---
+++ R/model_mrw.R
@@ -0,0 +1,16 @@
+#' Reproduce Regression Model Output MRW
+#' 
+#' Automatically recreates the output tables of the econometric models as formalized
+#' by Mankiw, Romer, Weil in their famous 1992 paper 'A Contribution to the Emperics
+#' of Economic Growth'.
+#'
+#' @param table 
+#' @param new_data 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+model_mrw <- function(table, new_data = FALSE) {
+  
+}
